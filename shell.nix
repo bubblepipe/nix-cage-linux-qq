ea27569044c52e1a7a25321f3000f8160543e017
nagisakaworu <nag1sakaw0ru@riseup.net>
2024-12-30 14:00:02 +0800

    use 2411

--- shell.nix
+++ shell.nix
@@ -1,11 +1,14 @@
-let nixpkgs = <nixpkgs>; in
-with import nixpkgs {
-  config.allowUnfree = true;
-};
-stdenv.mkDerivation {
+let 
+  nixpkgs = <nixpkgs>; 
+  pkgs-2411 = import (builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11-beta.tar.gz"; }) {
+     config.allowUnfree = true;
+   } ;
+in
+
+pkgs-2411.stdenv.mkDerivation {
   name = "nix-shell";
   buildInputs = [
-    qq
+    pkgs-2411.qq
   ];
   shellHook = ''
     export NIX_PATH=nixpkgs=${nixpkgs}
