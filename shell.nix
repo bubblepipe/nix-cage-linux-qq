6fd76904b07a9a5878fe774d7029388ec4f7ca8b
bubblepipe <nag1sakaw0ru@riseup.net>
2024-12-30 22:45:49 +0800

    old version qq

--- shell.nix
+++ shell.nix
@@ -1,15 +1,39 @@
-let nixpkgs = <nixpkgs>; in
-with import nixpkgs {
-  config.allowUnfree = true;
-};
-stdenv.mkDerivation {
-  name = "nix-shell";
-  buildInputs = [
-    qq
+# let nixpkgs = <nixpkgs>; in
+# with import nixpkgs {
+#   config.allowUnfree = true;
+# };
+# stdenv.mkDerivation {
+#   name = "nix-shell";
+#   buildInputs = [
+#     qq
+#     xorg.xhost
+#   ];
+#   shellHook = ''
+#     export NIX_PATH=nixpkgs=${nixpkgs}
+#     unset TZ
+#     xhost +
+#     qq
+#   '';
+# }
+
+
+{ pkgs ? import <nixpkgs> { config.allowUnfree = true;} 
+, pkgs-old-qq ? import 
+    # ( builtins.fetchGit { url = "https://github.com/nixos/nixpkgs.git"; rev = "71a6392e367b08525ee710a93af2e80083b5b3e2" ; }  # too slow
+    ( builtins.fetchTarball { url = "https://github.com/nixos/nixpkgs/archive/71a6392e367b08525ee710a93af2e80083b5b3e2.tar.gz"; }
+    ) { config.allowUnfree = true;}  }:
+
+pkgs.mkShell
+{
+  nativeBuildInputs = [
+    pkgs-old-qq.qq
+    pkgs.xorg.xhost
   ];
+
   shellHook = ''
-    export NIX_PATH=nixpkgs=${nixpkgs}
+    touch ~/.zshrc
     unset TZ
+    xhost +
     qq
   '';
 }
